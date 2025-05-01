b9616f9f75273c31737990f2f79ac332bf0ea41d
SYED NAHIDA ANJUM <snahidaanjum@gmail.com>
2025-05-01 17:01:38 +0530

    Adding logout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
+import Navbar from '../components/Navbar';
 import TrailerList from '../components/TrailerList';
 import '../styles/Home.css';
 
 export default function Home() {
   return (
     <div className="home-container">
+      <Navbar />
       <main>
         <TrailerList />
       </main>
